b9a347c813ff00f2fc9964993711566b5f2390ec
Lucas Lima <lucasViniLima15@gmail.com>
2024-09-14 16:46:59 -0300

    feat: adicionando view para criar usuário

--- reservas/users/views.py
+++ reservas/users/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from rest_framework import generics
+from django.contrib.auth.models import User
 
-# Create your views here.
+from .serializers import UserSerializer
+
+class UserCreateView(generics.CreateAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
